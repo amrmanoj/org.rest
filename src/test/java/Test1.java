b18aabcf8b9091e1ec5a5ab6c63f85c13d8c2f82
govind <govind1999rao@gmail.com>
2023-03-16 18:42:20 +0530

    first commit

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -3,6 +3,7 @@ public class Test1
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("Hello GitHub");
+        System.out.println("hii bro i got ur project through git commands")
     }
 }
 
